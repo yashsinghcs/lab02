1f238b1f2776bb596e37b935666ac0776773c3a9
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 14:54:36 -0700

    added scanner input and output as following question

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -6,6 +6,6 @@ public class Question2 {
         Scanner scanner = new Scanner(System.in);
         String value1 = scanner.next().trim();
         String value2 = scanner.next().trim();
-        System.out.println(value1 + " UNIVERSITY" + value2);
+        System.out.println(value1 + " UNIVERSITY " + value2);
     }
 }
---
+++ src/question3/Question3.java
@@ -0,0 +1,17 @@
+package question3;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value1 = scanner.nextInt();
+        int value2 = scanner.nextInt();
+        System.out.println(value1 + "+" + value2 + "=" + (value1 + value2));
+        System.out.println(value1 + "-" + value2 + "=" + (value1 - value2));
+        System.out.println(value1 + "*" + value2 + "=" + (value1 * value2));
+        System.out.println(value1 + "/" + value2 + "=" + (value1 / value2));
+        System.out.println(value1 + "%" + value2 + "=" + (value1 % value2));
+
+    }
+}
