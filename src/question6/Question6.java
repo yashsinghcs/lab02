f48cdb60269921b2b3a08f0cc5bd81c50b8935ee
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 15:33:17 -0700

    final commit

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char value1 = scanner.nextLine().trim().charAt(0);
-        char value2 = scanner.nextLine().trim().charAt(0);
+        char value1 = scanner.next().trim().charAt(0);
+        char value2 = scanner.next().trim().charAt(0);
 
 
         if (Character.toLowerCase(value1) > Character.toLowerCase(value2)) {
