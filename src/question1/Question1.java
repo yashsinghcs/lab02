0acfc156401301d3b24db12d0ee46db72df5ab20
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 14:18:56 -0700

    add input method : Scanner

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -1,8 +1,11 @@
 package question1;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
 
     }
 }
