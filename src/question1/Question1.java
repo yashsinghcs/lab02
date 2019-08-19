bc5538850e400cd08e26c986fe1e5e29a3bd55cd
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 14:31:17 -0700

    Final commit

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -6,7 +6,7 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
-        System.out.println("Welcome " + name + "!");
+        System.out.println("Welcome, " + name + "!");
         input.close();
     }
 }
