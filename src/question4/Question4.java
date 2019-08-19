a05f0523b22540050a7541577338342f28fccbf6
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 15:07:23 -0700

    added scanner input and output as following question and finally question four done and executed correctly

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        if (value > 0) {
+            System.out.println("Positive");
+        } else if (value < 0) {
+            System.out.println("Negative");
 
+        } else {
+            System.out.println("Zero");
+        }
     }
 }
