e9cd5beb051fc310a55ce6cb1056313ba430a630
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-19 15:32:09 -0700

    added scanner input and output as following question and finally question four done and executed correctly

---
+++ src/question6/Question6.java
@@ -0,0 +1,19 @@
+package question6;
+
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char value1 = scanner.nextLine().trim().charAt(0);
+        char value2 = scanner.nextLine().trim().charAt(0);
+
+
+        if (Character.toLowerCase(value1) > Character.toLowerCase(value2)) {
+            System.out.println(value2 + " " + value1);
+
+        } else {
+            System.out.println(value1 + " " + value2);
+        }
+    }
+}
