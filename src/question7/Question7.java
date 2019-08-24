069bb79bcf5a0b0f27c33003079cf13731ce13f9
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-24 13:25:26 -0700

    added main.java: added if-else statement and thus had desired output of the progrm

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -1,7 +1,19 @@
 package question7;
 
+import java.util.Scanner;
+
 public class Question7 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char value = input.nextLine().charAt(0);
+        if (value > 'a' && value < 'z' || value > 'A' && value < 'Z') {
+            System.out.println("alphabet");
+        } else if (value > '0' & value < '9') {
+            System.out.println("digit");
+        } else {
+            System.out.println("special character");
+        }
+
 
     }
 }
