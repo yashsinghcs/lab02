e986ef1d6f94e786853f5c5b4e9860a8442e5837
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-24 20:32:32 -0700

    question 11: Question11 solved

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -1,7 +1,18 @@
 package question11;
 
+import java.util.Scanner;
+
 public class Question11 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int srange = input.nextInt();
+        int erange = input.nextInt();
+        for (int i = srange + 1; i < erange + 1; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+
+        }
 
     }
 }
