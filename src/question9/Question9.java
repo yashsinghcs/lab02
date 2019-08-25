1c4a6c7d43fb78aead6e5eed1fc802ce26764ba7
YASH KUMAR SINGH <yash.singh_cs18@gla.ac.in>
2019-08-24 19:54:35 -0700

    question 9 solved using character object and toupperr 7 to lower

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,7 +1,18 @@
 package question9;
 
+import java.util.Scanner;
+
 public class Question9 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char value = input.nextLine().trim().charAt(0);
+        if (Character.isLowerCase(value)) {
+            value = Character.toUpperCase(value);
+            System.out.println(value);
+        } else {
+            System.out.println(Character.toLowerCase(value));
+        }
+
 
     }
 }
