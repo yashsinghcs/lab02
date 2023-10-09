dab9a9cf7518f070e2204b6775b3b02d1f3baf9f
D.Sunil <sunilnitdgp5@gmail.com>
2023-10-09 17:17:25 +0530

    Update LIS.java
    Descriptive comments have been added to explain the purpose of each method and major code sections.
    Try-with-resources has been used to automatically close the Scanner instance when it's no longer needed, ensuring proper resource management.
    Error handling has been enhanced to catch InputMismatchException when non-integer input is provided, providing informative error messages.

--- Coding-Blocks-/Assignment-1/LIS.java
+++ Coding-Blocks-/Assignment-1/LIS.java
@@ -1,58 +1,69 @@
 import java.util.*;
 public class LIS {
-	public static int binarySearch(int[] le, int si, int ei, int item) {
+	/**
+     * Performs a binary search to find the correct position to insert an element in an array.
+     *
+     * @param lengthsEnding An array representing the lengths of increasing subsequences ending at each position.
+     * @param si            Start index for the search.
+     * @param ei            End index for the search.
+     * @param item          The element to be inserted.
+     * @return The index where the element should be inserted.
+     */
+    private static int binarySearch(int[] lengthsEnding, int si, int ei, int item) {
+        int low = si;
+        int high = ei;
 
-		int low = si;
-		int high = ei;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (item > lengthsEnding[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
 
-		while (low <= high) {
-
-			int mid = (low + high) / 2;
-			if (item > le[mid]) {
-				low = mid + 1;
-			} else {
-				high = mid - 1;
-			}
-
-		}
-
-		return low;
-
-	}
-	public static int LISBU(int[] arr) {
-
-		int[] le = new int[arr.length];
-
-		le[0] = arr[0];
-		int len = 1;
-
-		for (int i = 1; i < arr.length; i++) {
-
-			if (arr[i] > le[len - 1]) {
-				le[len] = arr[i];
-				len++;
-			} else {
-
-				int idx = binarySearch(le, 0, len - 1, arr[i]);
-				le[idx] = arr[i];
-			}
-
-		}
-
-		return len;
-
-	}
-    public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for(int i = 0;i < arr.length;i++){
-			arr[i] = sc.nextInt();
-		}
-		System.out.println(LISBU(arr));
+        return low;
     }
 
-	
+    /**
+     * Calculates the length of the Longest Increasing Subsequence (LIS) using bottom-up dynamic programming.
+     *
+     * @param arr An array of integers.
+     * @return The length of the LIS.
+     */
+    public static int calculateLISLength(int[] arr) {
+        int[] lengthsEnding = new int[arr.length];
+        lengthsEnding[0] = arr[0];
+        int len = 1;
 
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > lengthsEnding[len - 1]) {
+                lengthsEnding[len] = arr[i];
+                len++;
+            } else {
+                int idx = binarySearch(lengthsEnding, 0, len - 1, arr[i]);
+                lengthsEnding[idx] = arr[i];
+            }
+        }
+
+        return len;
+    }
+
+    public static void main(String args[]) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int maxLength = calculateLISLength(arr);
+            System.out.println("Length of Longest Increasing Subsequence: " + maxLength);
+        } catch (InputMismatchException e) {
+            // Handle input mismatch exception (e.g., non-integer input).
+            System.err.println("Invalid input. Please enter integers only.");
+        }
+    }
 	
 }
